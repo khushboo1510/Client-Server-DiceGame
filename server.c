d43a883226327510c4b9ae931d41bad90275a31a
khushboo1510 <khushboomehta1995@gmail.com>
2020-04-13 18:05:14 -0400

    implemented server-side dice game

--- server.c
+++ server.c
@@ -7,6 +7,9 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+
+void servicePlayers(int, int);
+
 int main(int argc, char *argv[]){  
 	int sd, portNumber, player1, player2;
 	struct sockaddr_in servAdd;     // server socket address
@@ -41,4 +44,55 @@ int main(int argc, char *argv[]){
 	}
 	
 	return 0;
+}
+
+void servicePlayers(int player1, int player2){
+
+	int player1Score[2], player2Score[2];
+  	player1Score[0]=0;
+  	player1Score[1]=0;
+  	player2Score[0]=0;
+  	player2Score[1]=0;
+	
+	while(1){
+    
+		printf("Referee: TOTO plays:\n");
+		write(player1, "You can now play",50);		//sends message to client 1 to play its turn
+		if(read(player1, &player1Score, sizeof(player1Score)) < 0){		//reads score from client 1
+			fprintf(stderr, "read() error\n");
+			exit(3);
+		}
+		
+		printf("Points earned: %d\n",player1Score[0]);		//prints points earned by client 1
+		printf("Total: %d\n",player1Score[1]);		//prints total points earned by client 1 so far
+		
+		if(player1Score[1] >= 100){		//checks if total score of client 1 reaches 100
+			printf("TOTO won\n");
+			write(player1,"Game over: You won the game",50);	//sends winning message to client 1
+			close(player1);		//closes client 1 socket
+			write(player2,"Game over: you lost the game",50);	//sends losing message to client 2	
+			close(player2);		//closes client 2 socket
+			exit(5);
+		}
+
+		printf("Referee: TITI plays:\n");
+		write(player2, "You can now play",50);		//sends message to client 2 to play its turn
+		if(read(player2, &player2Score, sizeof(player2Score)) < 0){		//reads score from client 2
+			fprintf(stderr, "read() error\n");
+			exit(3);
+		}
+		
+		printf("Points earned: %d\n",player2Score[0]);		//prints points earned by client 2
+		printf("Total: %d\n",player2Score[1]);		//prints total points earned by client 2 so far
+		
+		if(player2Score[1] >= 100){		//checks if total score of client 2 reaches 100
+			printf("TITI won\n");
+			write(player2,"Game over: you won the game",50);	//sends winning message to client 2
+			close(player2);		//closes client 2 socket
+			write(player1,"Game over: you lost the game",50);	//sends losing message to client 2
+			close(player1);		//closes client 1 socket
+			exit(5);
+		}		
+  }
+    exit(1); 
 }
